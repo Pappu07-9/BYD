0bff93004fd181038ab1d1e8c88a799c20960f92
Pappu07-9 <prakharbhandari2002@gmail.com>
2024-01-22 03:51:04 +0545

    Added username in homepage

--- callcenter/views.py
+++ callcenter/views.py
@@ -4,21 +4,3 @@ from django.contrib.messages import constants as messages
 from django.contrib.auth import authenticate, login
 # Create your views here.
 
-def homepage(request):
-    if request.method == 'post':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        if User.objects.filter(username = username).exists():
-            messages.ERROR(request, 'Invalid Username')
-            return redirect('login')
-        
-        user = authenticate(username = username , password = password)
-
-        if user is None:
-            messages.ERROR(request, 'Invalid Password')
-            return redirect('login')
-        else:
-            return redirect('adddata')
-    return render(request,'login/login.html')
-    # return render(request, "userhomepage/index.html")
